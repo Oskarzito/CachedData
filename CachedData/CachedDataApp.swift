5353d5488b3e1bab86fdf52317b6c854fbbc5f67
Oskar Emilsson <oskar.emilsson@bontouch.com>
2025-03-10 11:55:47 +0100

    Initial Commit

---
+++ CachedData/CachedDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CachedDataApp.swift
+//  CachedData
+//
+//  Created by Oskar Emilsson on 2025-03-10.
+//
+
+import SwiftUI
+
+@main
+struct CachedDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CachedDataTests/CachedDataTests.swift
@@ -0,0 +1,17 @@
+//
+//  CachedDataTests.swift
+//  CachedDataTests
+//
+//  Created by Oskar Emilsson on 2025-03-10.
+//
+
+import Testing
+@testable import CachedData
+
+struct CachedDataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
